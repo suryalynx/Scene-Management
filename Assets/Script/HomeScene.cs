44a59226f952380dab063dfe1c9c0bdb0ad7aedf
Surya Herdianto Utomo <48855587+suryalynx@users.noreply.github.com>
2024-02-29 12:05:19 +0800

    first commit to remote
    commit semua

---
+++ Assets/Script/HomeScene.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class HomeScene : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text highScore;
+
+    private void Awake()
+    {
+        //Set highscore text using 3 method
+
+        // method 1 : Static Variable
+        //highScore.text = StaticData.HighScore.ToString();
+
+        // method 2 : Static Object
+        //highScore.text = GameData.Data.HighScore.ToString();
+
+        // method 3 : Singleton
+        highScore.text = SingletonData.Instance.HighScore.ToString();
+    }
+
+    public void GotoPlayScene(){
+        Debug.Log("Go to Play Scene");
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    
+}
